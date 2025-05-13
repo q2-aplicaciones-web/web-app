605abf04c1b32394530becd452bc04d845adee9f
fabriziocpa <fabriziocpa1516@gmail.com>
2025-05-13 10:48:10 -0500

    feat: add router configuration with routes for home, dashboard, design lab, and project details

---
+++ src/router/index.js
@@ -0,0 +1,56 @@
+import HomeComponent from "../public/pages/home.component.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import CreateProjectComponent from "../design-lab/components/create-project.component.vue";
+import DashboardComponent from "../public/pages/dashboard.component.vue";
+
+const DesignLabComponent = () =>
+    import("../public/pages/design-lab.component.vue");
+const ProjectDetailComponent = () =>
+    import("../design-lab/components/project-detail.component.vue");
+
+const routes = [
+    {
+        path: "/home",
+        name: "home",
+        component: HomeComponent,
+        meta: { title: "Home" },
+    },
+    {
+        path: "/dashboard",
+        name: "dashboard",
+        component: DashboardComponent,
+        meta: { title: "Dashboard" },
+    },
+    {
+        path: "/design-lab",
+        name: "design-lab",
+        component: DesignLabComponent,
+        meta: { title: "Design Lab" },
+    },
+    {
+        path: "/design-lab/new",
+        name: "create-project",
+        component: CreateProjectComponent,
+        meta: { title: "Create Project" },
+    },
+    {
+        path: "/design-lab/:id",
+        name: "project-detail",
+        component: ProjectDetailComponent,
+        meta: { title: "Project Detail" },
+    },
+    { path: "/", name: "default", redirect: "/home" },
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.BASE_URL),
+    routes: routes,
+});
+
+router.beforeEach((to, from, next) => {
+    let baseTitle = "Q2";
+    document.title = `${baseTitle} | ${to.meta["title"]}`;
+    next();
+});
+
+export default router;
